2bda25f3d27f54eedac9a53f13a4d3cfbc59448d
muhamash <ashraful@xpeedlab.tech>
2024-02-02 03:32:38 +0600

    footer,search,headerTitle

---
+++ src/components/SelectField.jsx
@@ -0,0 +1,21 @@
+const SelectField = () =>
+{
+    return (
+        <>
+            {/* <!-- Sort --> */}
+            <select
+                className="cursor-pointer rounded-md border px-4 py-2 text-center text-gray-600"
+                name="sortBy"
+                id="sortBy"
+            >
+                <option value="">Sort</option>
+                <option value="name_asc">Name (A-Z)</option>
+                <option value="name_desc">Name (Z-A)</option>
+                <option value="year_asc">Publication Year (Oldest)</option>
+                <option value="year_desc">Publication Year (Newest)</option>
+            </select>
+        </>
+    );
+};
+
+export default SelectField;
